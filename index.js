74454e9db5dd1f8b79d7e8c3095e9b2f4636859a
haruntasci <haruntasci77@gmail.com>
2023-02-15 00:10:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//Constructor Funtion
+function Circle(radius) {
+    this.radius = radius
+    this.draw = function () {
+        console.log("draw")
+    }
+}
+const circle = new Circle(1)
+circle.draw()
